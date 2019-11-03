39a0d3e04cd3052dc27644e938542a68d6a65b94
Eslavon <46901350+eslavon@users.noreply.github.com>
2019-11-03 13:21:07 +0300

    Работа с сообщениями

---
+++ example/example_message_method.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once "../vendor/autoload.php";
+
+use Easybot\Core\VkMessageMethod;
+
+$user_id = 251510315;
+$peer_id = 251510315;
+$token = "f3ffe56dedb4ed341b2169b00a33f0759c6e8b0323bae19295ff3fa6216bcde9c296aa908af1208fd93ce";
+$version = "5.103";
+
+$vk_message = new VkMessageMethod($token,$version);
+
+// Отправка сообщения универсальным методом
+//$params = array("peer_id" => $peer_id,"message" => "Hello Word");
+//$result = $vk_message->messagesSend($params);
+
+//Отправка простого текстового сообщения
+//$result = $vk_message->sendMessage($peer_id,"Hello!");
+//var_dump($result);
+
+//Отправка простого текстового сообщения c дополнительными параметрами
+//$setting = array(
+	//"dont_parse_links" => 1, // Не создавать снипет ссылки
+	//"disable_mentions" => 1, // Отключить уведомление
+	//"intent" => "promo_newsletter" // Интент релкламной рассылки
+//);
+//$result = $vk_message->sendMessage($peer_id,"Hello!",$setting);
+
+//Отправка текстового соообщения c геоточкой
+//$result = $vk_message->sendMessageGeo($peer_id,"Location",40.34,34.35);
+
+// Отправка клавиатуры (не скрываеться после нажатия)
+//$button1 = ["command","Зеленая кнопка","positive","text"];
+//$button2 = ["command","Красная кнопка","negative","text"];
+//$button3 = ["command","Синяя кнопка","primary","text"];
+//$button4 = ["command","Белая кнопка","secondary","text"];
+//$button5 = ["command","Местоположение","default","location"];
+//$button6 = ["command","VK Pay","default","vkpay","action=transfer-to-group&group_id=1&aid=10"];
+//$button7 = ["command","Vk APPS","default","open_app",6979558,-181108510,"sendKeyboard"];
+//$button = [[$button1],[$button2],[$button3],[$button4],[$button5],[$button6],[$button7]];
+//$result = $vk_message->sendMessageButton($peer_id,"Hello",$button);
+
+// Отправка кнопок в режиме inline
+$setting = array(
+	"dont_parse_links" => 1, // Не создавать снипет ссылки
+	"disable_mentions" => 1, // Отключить уведомление
+);
+$button1 = ["command","Зеленая кнопка","positive","text"];
+$button2 = ["command","Красная кнопка","negative","text"];
+$button3 = ["command","Синяя кнопка","primary","text"];
+$button = [[$button1],[$button2],[$button3]];
+$result = $vk_message->sendMessageButton($peer_id,"Hello",$button,"inline",$setting);
+var_dump($result);
+
--- src/core/VkMessageMethod.php
+++ src/core/VkMessageMethod.php
@@ -41,4 +41,168 @@ class VkMessageMethod extends VkRequest
         $this->token = $token;
         $this->version = $version;
     }
+
+    /**
+     * Генерация случайного числа
+     *
+     * @return int
+     */
+    private function random()
+    {
+        $random = mt_rand(-99999999,99999999);
+        return $random;
+    }
+
+   /**
+     * Формируем клавиатуру из массива
+     *
+     * @param array $array - массив клавиатуры
+     * @param mixed $setting - Скрывать ли клавиатуру после нажатия, включить ли режим inline
+     *
+     * @return string
+     */
+    private function formationKeyboard($array,$setting = null)
+    {
+        if ($setting == null) {
+            $one_time = false;
+            $inline = false;
+        } elseif ($setting == "inline") {
+            $one_time = false;
+            $inline = true;
+        } elseif ($one_time == true) {
+            $one_time = true;
+            $inline = false;
+        }
+
+        $keyboard = [];
+        $x = 0;
+        foreach ($array as $buttons) {
+            $y = 0;
+            foreach ($buttons as $button) {
+                $keyboard[$x][$y]["action"]["type"] = $button[3];
+
+                if ($button[3] == "text") {
+                    $keyboard[$x][$y]["action"]["label"] = $button[1];
+                    $keyboard[$x][$y]["color"] = $button[2];
+                } elseif ($button[3] == "vkpay") {
+                    $keyboard[$x][$y]["action"]["hash"] = $button[4];
+                } elseif ($button[3] == "open_app") {
+                    $keyboard[$x][$y]["action"]["label"] = $button[1];
+                    $keyboard[$x][$y]["action"]["app_id"] = $button[4];
+                    $keyboard[$x][$y]["action"]["owner_id"] = $button[5];
+                    $keyboard[$x][$y]["action"]["hash"] = $button[6];
+                }
+                
+                $keyboard[$x][$y]["action"]["payload"] = '{"command": "'.$button[0].'"}';
+
+                $y++;
+            }
+            $x++;
+        }
+            $array_keyboard = ["one_time" => $one_time,"buttons" => $keyboard,"inline"=> $inline];
+            $json_keyboard = json_encode($array_keyboard, JSON_UNESCAPED_UNICODE);
+            return $json_keyboard;
+    }
+
+    /**
+     * Отправка сообщения (Универсальный метод)
+     *
+     * @param array $array_params - массив параметров
+     *
+     * @return array
+     */
+    public function messagesSend($array_params)
+    {
+        $params = array(
+            "access_token" => $this->token,
+            "v" => $this->version            
+        );
+        foreach ($array_params as $key => $value) {
+            $params[$key] = $value;
+        }
+        $params["random_id"] = $this->random();
+        $method = "messages.send";
+        $result = $this->request($method,$params);
+        return $result;
+    }
+
+    /**
+     * Отправка текстового соообщения пользователю/в беседу
+     *
+     * @param int $peer_id - ID назначения
+     * @param string $message - Текст сообщения
+     * @param array $setting - Параметры (Сниппет ссылки,Уведомление,Интент)
+     *
+     * @return array
+     */
+    public function sendMessage($peer_id,$message,$setting = null)
+    {
+        $params = array(
+            "peer_id" => $peer_id,
+            "message" => $message
+        );
+        if ($setting !== null) {
+            foreach ($setting as $key => $value) {
+                $params[$key] = $value;
+            }
+        }
+        $result = $this->messagesSend($params);
+        return $result;
+    }
+
+    /**
+     * Отправка текстового соообщения c геоточкой
+     *
+     * @param int $peer_id - ID назначения
+     * @param string $message - Текст сообщения
+     * @param float $lat - географическая широта (от -90 до 90).
+     * @param float $long - географическая долгота (от -180 до 180).
+     * @param array $setting - Параметры (Сниппет ссылки,Уведомление,Интент)
+     *
+     * @return array
+     */
+    public function sendMessageGeo($peer_id,$message,$lat,$long,$setting = null)
+    {
+        $params = array(
+            "peer_id" => $peer_id,
+            "message" => $message,
+            "lat" => $lat,
+            "long" => $long
+        );
+        if ($setting !== null) {
+            foreach ($setting as $key => $value) {
+                $params[$key] = $value;
+            }
+        }
+        $result = $this->messagesSend($params);
+        return $result;
+    }
+
+    /**
+     * Отправка сообщения с клавиатурой
+     *
+     * @param int $peer_id - ID назначения
+     * @param string $message - Текст сообщения
+     * @param array $button - массив кнопок
+     * @param mixed $one_time - true - скрывать клавиатуру после нажатия, false - нет, inline -режим inline.
+     * @param array $setting - Параметры (Сниппет ссылки,Уведомление,Интент)
+     *
+     * @return array
+     */
+    public function sendMessageButton($peer_id,$message,$button,$one_time = null,$setting = null)
+    {
+        $params = array(
+            "message" => $message,
+            "peer_id" => $peer_id,
+            "keyboard" => $this->formationKeyboard($button,$one_time)
+            );
+        if ($setting !== null) {
+            foreach ($setting as $key => $value) {
+                $params[$key] = $value;
+            }
+        }       
+        $result = $this->messagesSend($params);
+        return $result;
+    }
 }
+
