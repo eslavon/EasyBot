eff9e2c305e8dd5c7cb07ec8c4e564e0596ffdae
Eslavon <46901350+eslavon@users.noreply.github.com>
2019-11-04 12:17:11 +0300

    Метод для загрузки изображений и документов

--- src/VkRequest.php
+++ src/VkRequest.php
@@ -10,6 +10,8 @@
 
 namespace Eslavon\Easybot;
 
+use CURLFile;
+
 /**
  * VkRequest - отправка запросов к VK_API
  */
@@ -20,6 +22,7 @@ class VkRequest extends EasybotException
      *
      * @param string $method - метод API VK
      * @param array $params - параметры запроса к API
+     *
      * @return array
      *
      * @throws EasybotException
@@ -49,5 +52,43 @@ class VkRequest extends EasybotException
         }
 		return $data;
 	}
+
+    /**
+     * Загрузка файла на сервер
+     *
+     * @param string $peer_id - ID назначения
+     * @param string $file - Локальный путь до файла
+     * @return string $upload_url - URL сервера для загрузки
+     *
+     * @return array
+     *
+     * @throws EasybotException
+     */
+    public function upload($peer_id,$file,$upload_url)
+    {
+        $post = [ "file" => new CURLFile(realpath($file)) ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $upload_url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $result = curl_exec($ch);
+        if ($result === FALSE) {
+            $error = curl_error($curl);
+            throw new EasybotException($error);
+        }       
+        curl_close($ch);  
+        $data = json_decode($result,true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $error = "JSON ERROR: ".json_last_error_msg();
+            throw new EasybotException($error);
+        }
+        if (isset($data["error"])) {
+            $error = $data["error"]["error_msg"];
+            throw new EasybotException($error);
+        }
+        return $data;     
+    }
 }
 
